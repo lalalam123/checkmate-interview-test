f6b34be4c3a9be06b76cc35ffa1e183698aaf8a8
lalalam123 <90650499+lalalam123@users.noreply.github.com>
2023-04-24 01:01:54 +0200

    Complete: All tasks

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,28 @@
 import Head from 'next/head'
 import GoogleButton from 'react-google-button'
-import {getAuth, GoogleAuthProvider} from "firebase/auth";
-import {useRouter} from "next/router";
+import { getAuth, GoogleAuthProvider, signInWithRedirect, getRedirectResult } from "firebase/auth";
+import { useRouter } from "next/router";
 import { initializeApp } from 'firebase/app';
 
 // Task 0: Initialize Firebase
 // Replace the following with your app's Firebase project configuration
 // https://firebase.google.com/docs/web/setup
 const firebaseConfig = {
-  // Enter your own firebase config here
+  apiKey: "AIzaSyAdl5e5zAirp0plnxn9Y6qM98RhaQ9q6BY",
+  authDomain: "checkmate-interview-test-7d6c1.firebaseapp.com",
+  projectId: "checkmate-interview-test-7d6c1",
+  storageBucket: "checkmate-interview-test-7d6c1.appspot.com",
+  messagingSenderId: "840843882620",
+  appId: "1:840843882620:web:3bd87de30442f4a90119f9",
+  measurementId: "G-W1Y3P33CEY"
 };
 
-// const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
 
 // GoogleAuthProvider instance
-// const provider = new GoogleAuthProvider();
+const provider = new GoogleAuthProvider();
 // Firebase Auth instance
-// const auth = getAuth(app);
+const auth = getAuth(app);
 
 export default function Home() {
   //Next.js router
@@ -24,15 +30,28 @@ export default function Home() {
 
   // Task 1: Implement Google Sign in with Firebase
   // https://firebase.google.com/docs/auth/web/google-signin
-  const signIn = () => {
+  const signIn = async () => {
     /*
       1. Use the GoogleAuthProvider to sign in with Firebase
       2. Use signInWithRedirect to redirect the user to the Google sign in page
       3. (Optional) Use getRedirectResult to get the result of the redirect and check out what is inside :)
       4. Redirect the user to the signed-in page using Next.js router
      */
-
+    await signInWithRedirect(auth, provider);
   }
+
+  getRedirectResult(auth)
+    .then((result) => {
+
+      const user = result?.user;
+
+      if (user) {
+        router.push('/signed-in')
+      }
+
+    }).catch((error) => {
+      console.log(error)
+    });
 
   return (
     <>
@@ -41,7 +60,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <div className="container">
-        <main style={{display: 'flex', justifyContent:'center', alignItems:'center', flexDirection: 'column'}}>
+        <main style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
           <h1 className="title">
             Welcome to <a href="https://checkmatehk.io">CheckMate</a>
           </h1>
@@ -52,11 +71,11 @@ export default function Home() {
           <GoogleButton
             label={'Sign in with Google'}
             type="light"
-            style={{ width: '50%', display:"flex", justifyContent: 'center', alignItems: 'center', fontFamily: 'Roboto, sans-serif', color:'#444' }}
+            style={{ width: '50%', display: "flex", justifyContent: 'center', alignItems: 'center', fontFamily: 'Roboto, sans-serif', color: '#444' }}
             onClick={signIn}
           />
         </main>
       </div>
-      </>
+    </>
   )
 }
--- src/pages/signed-in.tsx
+++ src/pages/signed-in.tsx
@@ -1,12 +1,23 @@
-import {GetServerSideProps, InferGetServerSidePropsType} from "next";
+import { GetServerSideProps, InferGetServerSidePropsType } from "next";
+import { getFirestore, doc, getDoc } from "firebase/firestore";
+import { app } from "./index";
 
 export default function SignedIn({ joke }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <div>
       <h1>Signed In</h1>
-      {/* Task 3: Your own presentation of the joke here (Free Style 😉 )*/}
 
+      {/* Task 3: Your own presentation of the joke here (Free Style 😉 )*/}
+      <div >
+        <h1 >Fetch random joke by API</h1>
+        <p>{joke.data[0].setup + ' ' + joke.data[0].punchline}</p>
+      </div>
+      <div >
+        <h1 >Fetch random joke from Firebase DB</h1>
+        <p >{joke.data_db}</p>
+      </div>
       {/* End of Task 3 */}
+
     </div>
   )
 
@@ -15,12 +26,20 @@ export default function SignedIn({ joke }: InferGetServerSidePropsType<typeof ge
 // Task 2: Fetch random jokes from the API
 // https://official-joke-api.appspot.com/jokes/programming/random
 export const getServerSideProps: GetServerSideProps = async (context) => {
+  // Fetch programming jokes from the Firebase Realtime Database
+  const db = getFirestore(app);
+  const docRef = doc(db, "jokes", "cWZ6lr6rPWLCdcpiYJTB");
+  const docSnap = await getDoc(docRef);
+
   // Fetch data from external API and pass it to the page via props.joke
+  const response = await fetch('https://official-joke-api.appspot.com/jokes/programming/random');
+  const data_fetch = await response.json();
 
   return {
     props: {
       joke: {
-
+        data_db: docSnap.data()?.joke,
+        data: data_fetch,
       },
     }, // will be passed to the page component as props
   }
